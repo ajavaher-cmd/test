790149433cb1e0b5b5dfaa540258b1048e997718
Ali <ajavaher@ualberta.ca>
2021-05-11 01:24:25 +0430

    first

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import TradingViewWidget from 'react-tradingview-widget';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <TradingViewWidget symbol="NASDAQ:AAPL" />
+       
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
