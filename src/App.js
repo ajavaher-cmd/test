2d6f8133e4ea173e48e4f9e1fc76260c5855fd34
Ali <ajavaher@ualberta.ca>
2021-05-12 20:58:33 +0430

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 
 import './App.css';
 import TradingViewWidget, { Themes } from 'react-tradingview-widget';
-import React, { useState } from 'react';
+import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
 import List from '@material-ui/core/List';
 import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
